6a1c533481470ab0eb778963ba312ecf8cbe89ad
Thierry Morre <thierry.morre@cognitic.be>
2024-09-26 14:31:35 +0200

    Ajout du Trigger

--- SampleDatabase/Tables/Student.sql
+++ SampleDatabase/Tables/Student.sql
@@ -11,3 +11,14 @@
   CONSTRAINT UK_Student_Login UNIQUE ([Login]),
   CONSTRAINT CK_Student_YearResult CHECK (YearResult BETWEEN 0 AND 20)
 );
+GO
+
+CREATE TRIGGER CTR_OnDeleteStudent
+ON Student
+INSTEAD OF DELETE
+AS
+BEGIN
+	UPDATE Student SET Active = 0
+	WHERE Id IN (SELECT Id FROM deleted)
+	AND Active = 1;
+END
